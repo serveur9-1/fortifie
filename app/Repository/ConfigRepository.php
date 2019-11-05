503f32d292fda2f8357eca833313d7cfe297c391
Mousouba <mousouba@nan.ci>
2019-11-05 11:28:05 +0000

    Config part added!

---
+++ app/Repository/ConfigRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace App\Repository;
+
+
+use App\Config;
+
+class ConfigRepository
+{
+    private $c;
+
+    public function __construct(Config $c)
+    {
+        $this->c = $c;
+    }
+
+
+
+
+    public function getAllConfig()
+    {
+        return $this->c->newQuery()->select()->get();
+    }
+
+
+    public function updateConfig($id, $array)
+    {
+        $v = $this->c->newQuery()->findOrFail($id);
+
+        $v->update([
+            'telephone' => $array['telephone'],
+            'email' => $array['email'],
+            'localite' => $array['localite']
+        ]);
+    }
+
+
+
+
+}
